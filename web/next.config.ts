9c035fc4881e0715b2fff1f753705563720c2edf
anathan <ilhamadian346@gmail.com>
2026-04-25 16:11:52 +0700

    fix: suppress metamask react-native warning and speed up compile

--- web/next.config.ts
+++ web/next.config.ts
@@ -3,6 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "@react-native-async-storage/async-storage": false,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
